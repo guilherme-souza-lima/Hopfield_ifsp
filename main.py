f8028d2b0beade83fe8eafe17fb84b063aa4011a
guizoio <zoio.182@hotmail.com>
2022-06-14 01:24:07 -0300

    feat::white_check_mark::

--- main.py
+++ main.py
@@ -147,8 +147,12 @@ def main():
         if re.match(r'[0-9a-zA-Z-_]*.jp[e]*g', i):
             test_paths.append(path+i)
 
-    #A REDE HOPFIELD COMELA
-    hopfield(train_files=train_paths, test_files=test_paths, theta=0.5, time=20000, size=(100, 100), threshold=60, current_path=current_path)
+    #A REDE HOPFIELD COMECA
+    hopfield(train_files=train_paths, test_files=test_paths, theta=1.9, time=200000, size=(100, 100), threshold=70,current_path=current_path)
+    # ALGUNS TESTES:
+    # hopfield(train_files=train_paths, test_files=test_paths, theta=0.1, time=1000, size=(100, 100), threshold=70, current_path=current_path)
+    # hopfield(train_files=train_paths, test_files=test_paths, theta=1.9, time=5000, size=(100, 100), threshold=70, current_path=current_path)
+
     #PRINT PARA SABER O TEMPO QUE LEVOU PARA EXECUTAR A APLICAÇÃO
     print("--- %s Segundos ---" % (time.time() - start_time))
 
