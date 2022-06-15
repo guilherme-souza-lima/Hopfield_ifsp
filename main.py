4ac42783ecfac9e840a9adda3951acd3d30ea7fb
guilherme-souza-lima <guilherme_souza.lima@hotmail.com>
2022-06-15 11:49:13 -0300

    feat::sparkles::include config

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ import os
 import time
 import hopfield
 import settings
+import config
 
 ##INICIO DO CODIGO
 def main():
@@ -11,7 +12,7 @@ def main():
     test_paths = settings.testPath(current_path)
 
     #A REDE HOPFIELD COMECA
-    hopfield.hopfield(train_files=train_paths, test_files=test_paths, theta=1.9, time=200000, size=(100, 100), threshold=70, current_path=current_path)
+    hopfield.hopfield(train_files=train_paths, test_files=test_paths, theta=config.THETA, time=config.TIME, size=(100, 100), threshold=config.THESHOLD, current_path=current_path)
 
     #PRINT PARA SABER O TEMPO QUE LEVOU PARA EXECUTAR A APLICAÇÃO
     print("--- %s Segundos ---" % (time.time() - start_time))
